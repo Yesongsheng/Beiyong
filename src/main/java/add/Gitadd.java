66b3e65d42233f9ca9a82baec0c2f2b8c05ac6fc
qwe <1256805764@qq.com>
2020-09-28 19:59:08 +0800

    111

---
+++ src/main/java/add/Gitadd.java
@@ -0,0 +1,10 @@
+package add;
+
+public class Gitadd {
+	private void syso() {
+		// TODO Auto-generated method stub
+			System.out.println("4444");
+			System.out.println("5555");
+			System.out.println("6666");
+	}
+}
